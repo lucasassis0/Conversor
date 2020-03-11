fa8f701b4ca4ed6788dbd077355409a1bcd4319e
Lucas Assis <lucas.assis_@hotmail.com>
2020-03-11 09:43:03 -0300

    att

--- src/components/Conversor.js
+++ src/components/Conversor.js
@@ -35,11 +35,31 @@ export default class Conversor extends Component {
             <div className="conversor">
             
             <h2>{this.props.moedaA} para {this.props.moedaB}</h2>
-            <input type="text" onChange={(event)=>{this.setState({moedaA_valor:event.target.value})}}></input>
-            <input type="button" value="Converter" onClick={this.converter}></input>
+            <input type="text" id="value" onChange={(event)=>{this.setState({moedaA_valor:event.target.value})}}></input>
+            {/*<input type="text" id="value" onChange={()=>''}></input>*/}
+            <input type="button" id = "submit" value="Converter" onClick={this.converter}></input>
+            
             <h2>{this.state.moedaB_valor}</h2>    
             
             </div>
         )
     }
 }
+
+/*var meuInput = document.getElementById('idDoSeuInput');
+var meuForm = document.getElementById('idDoSeuForm');
+
+input.onkeypress = teclaPressionada();
+
+function teclaPressionada(event) {
+    if(event.keyCode == 13) {
+        converter()
+    }
+}
+
+<script>
+                var text = getElementById("text")
+                text.addEventListener("keypress", function (evento){
+                    evento.keycode == 13 ? this.converter:0
+                })
+            </script>*/
